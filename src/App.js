f4a6f1c71c261742135e0bc11ae557afe4a091b4
khamais-maaroufi <khamais.sil@gmail.com>
2022-10-26 17:34:12 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Paginator from "./Paginator";
+
+const App = () => {
+  const [CurrentP, setCurrentP] = useState(1);
+  const n = 5;
+  const arrayi = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "11",
+    "12",
+    "13",
+  ];
+
+  /*explaining Usability here 
+  *suppose you have an  array of objects you want to render 
+
+  1)the length of the array will be passed to the longeur param
+  2)create a currentP react hooks and passe it to the Paginator component
+  3) passe the number of object per page then slice your array according to 
+  the currentP hooks and the number of object per page you want 
+  4)use the code i wrote with Arrayi.slice.map to render your on array with the currentP hook
+  & the n display variable 
+  */
+  return (
+    <div className="App">
+      <div>
+        {arrayi
+          .slice(Number(CurrentP) * n - n, Number(CurrentP) * n)
+          .map((item) => (
+            <h1>{item}</h1>
+          ))}
+      </div>
+      <Paginator
+        longeur={arrayi.length}
+        CurrentP={CurrentP}
+        setCurrentP={(value) => {
+          setCurrentP(value);
+        }}
+        PerPage={n}
+      />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Paginator.js
@@ -0,0 +1,103 @@
+import React, { useState, useEffect } from "react";
+import "./Paginator.css";
+
+const Paginator = (props) => {
+  const [pages, setPages] = useState();
+  const { longeur, CurrentP, setCurrentP, PerPage } = props;
+  let nbPages = 0;
+  if (longeur !== 0) {
+    const rest = Number(longeur) % PerPage;
+    if (rest === 0) {
+      nbPages = Number(longeur) / PerPage;
+    } else {
+      nbPages = Number(longeur) / PerPage + 1;
+    }
+  }
+
+  const selected = {
+    backgroundColor: "#003f46",
+    transform: "scale(1.3)",
+    color: "white",
+  };
+
+  useEffect(() => {
+    let arr = [];
+    if (nbPages < 5) {
+      for (let i = 1; i <= nbPages; i++) {
+        arr.push(i);
+      }
+    } else if (nbPages <= 0) {
+      arr.push(0);
+    } else {
+      if (CurrentP > 2 && CurrentP < nbPages - 2) {
+        for (let i = CurrentP - 2; i <= Number(CurrentP) + 2; i++) {
+          arr.push(i);
+        }
+      } else if (CurrentP <= 2) {
+        for (let i = 1; i <= 5; i++) {
+          arr.push(i);
+        }
+      } else if (CurrentP >= nbPages - 2) {
+        for (let i = CurrentP - 2; i <= nbPages; i++) {
+          arr.push(i);
+        }
+      }
+    }
+
+    setPages(arr);
+  }, [CurrentP]);
+
+  const currentor = (e) => {
+    e.preventDefault();
+    // console.log(e.target.id);
+    setCurrentP(e.target.id);
+  };
+  const handelprev = () => {
+    if (CurrentP > 1) {
+      let varii = 0;
+      varii = Number(CurrentP) - 1;
+      setCurrentP(varii);
+    }
+  };
+  const handelNext = () => {
+    if (CurrentP < nbPages-1) {
+      let varii = 0;
+      varii = Number(CurrentP) + 1;
+      setCurrentP(varii);
+      
+    }
+  };
+  return (
+    <div className="paginator-container">
+      <div className="flache" onClick={handelprev}>
+        {" "}
+        <img
+          id="leftdds"
+          src="https://cdn-icons-png.flaticon.com/512/860/860790.png"
+          onClick={handelprev}
+        ></img>
+      </div>
+      {pages &&
+        pages.map((item) => (
+          <div
+            className="circle-pgin"
+            onClick={(e) => currentor(e)}
+            style={CurrentP == item ? selected : {}}
+            id={item}
+          >
+            {item}
+          </div>
+        ))}
+      <div className="flache" onClick={handelNext}>
+        {" "}
+        <img
+          id="rightfsf"
+          src="https://cdn-icons-png.flaticon.com/512/860/860828.png"
+          onClick={handelNext}
+        ></img>
+      </div>
+    </div>
+  );
+};
+
+export default Paginator;
